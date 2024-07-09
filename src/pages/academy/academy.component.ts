94954db3e491d6a36726e31f3c6e9fe97c4e7389
ugfire <ugfirecompany@gmail.com>
2024-07-09 12:41:13 +0300

    removed hapi birthday

--- src/pages/academy/academy.component.ts
+++ src/pages/academy/academy.component.ts
@@ -1,13 +1,32 @@
 import { Component } from '@angular/core';
 import { CoursesComponent } from "../../components/courses/courses.component";
+import Swal from 'sweetalert2';
+import { TestimonialComponent } from "../../components/testimonial/testimonial.component";
 
 @Component({
     selector: 'app-academy',
     standalone: true,
     templateUrl: './academy.component.html',
     styleUrl: './academy.component.css',
-    imports: [CoursesComponent]
+    imports: [CoursesComponent, TestimonialComponent]
 })
 export class AcademyComponent {
+whatsappme():void{
+    Swal.fire({ html: `
+        <div class="text-center">
+          <h1 class="text-center text-success display-1">
+            <i class="bi bi-whatsapp"></i>
+          </h1>
+          <p class="text-center">+256 773 913902</p>
+          <p class="text-center">
+           <a href="https://api.whatsapp.com/send?phone=+256773913902&text=Hello%20Alfred!" class="btn btn-success">Send me a message</a>
 
+          </p>
+        </div>
+      `,
+      showConfirmButton: false
+    }
+       
+    );
+}
 }

bde8df2b833ffd60d97afaf1144f5d4264e8689c
ugfire <ugfirecompany@gmail.com>
2024-08-03 08:39:12 +0300

    added bigezo cards software for download windows pc

--- src/pages/cardgenerator/cardgenerator.component.ts
+++ src/pages/cardgenerator/cardgenerator.component.ts
@@ -12,22 +12,14 @@ import Swal from 'sweetalert2';
 export class CardgeneratorComponent {
   showPricing:boolean=true;
   showForm:boolean=false;
- getFree(): void {
-    Swal.fire({
-      html: `
-        <div class="text-center">
-          <h3 class="text-center text-dark">
-          GET FREE VERSION
-          </h3>
-        <div class="d-none">   <h3 class="text-center"> <a class="btn btn-dark">Fill a Form</a> </h3>
-          <p class="text-center">OR</p>
-         ></div>
-          <p class="text-center">
-           <a href="https://api.whatsapp.com/send?phone=+256773913902&text=Hello%20Alfred!" class="btn btn-success">Order via Whatsapp</a>
-          </p
-        </div>
-      `,
-      showConfirmButton: false
-    });
+  getFree(): void {
+    const link = document.createElement('a');
+    link.href = 'https://drive.usercontent.google.com/download?id=1ZcDLZMysaZyQifJ8JnoMElznGxN4EL4W&export=download&authuser=0';
+    link.download = 'bigezocardssetup.exe';
+    link.target = '_blank';
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
   }
 }
+
